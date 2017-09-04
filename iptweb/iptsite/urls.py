70f2d4f85b4aea6301986143f6a1a4f242ca4be3
Joe Stubbs <jstubbs@tacc.utexas.edu>
2017-09-04 00:03:55 -0500

    Add admin and webterm

--- iptweb/iptsite/urls.py
+++ iptweb/iptsite/urls.py
@@ -7,8 +7,10 @@ urlpatterns = [
     url(r'^help$', views.help, name='help'),
     url(r'^compile$', views.compile, name='compile'),
     url(r'^terminal$', views.terminal, name='terminal'),
+    url(r'^webterm$', views.webterm, name='webterm'),
     url(r'^login$', views.login, name='login'),
     url(r'^logout$', views.logout, name='logout'),
     url(r'^create_account$', views.create_account, name='create_account'),
+    url(r'^admin$', views.admin, name='admin'),
     url(r'^$', views.login, name='login'),
 ]
